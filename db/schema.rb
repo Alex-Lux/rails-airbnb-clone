f4bd683b11eb57ba7705a66668a50d052c98d302
Alexandra Vidic <alexandravidic@yahoo.fr>
2017-08-15 11:09:35 +0100

    Changing type to category in Pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815100127) do
+ActiveRecord::Schema.define(version: 20170815100847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,8 +58,8 @@ ActiveRecord::Schema.define(version: 20170815100127) do
     t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "type"
     t.text "observations"
+    t.string "category"
     t.index ["pethome_id"], name: "index_pets_on_pethome_id"
   end
 
