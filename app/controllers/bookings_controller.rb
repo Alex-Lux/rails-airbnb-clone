4a180a8137a79bb44f94fd2257d5b81e2068d3fe
Fernando Morgado <github@fernandomorgado.eu>
2017-08-16 11:11:35 +0100

    added gem and started booking controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,36 @@
+class BookingsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
+
+# restaurants example
+
+def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new()
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant_id = params[:restaurant_id]
+    @restaurant = Restaurant.find(@review.restaurant_id)
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant_id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
